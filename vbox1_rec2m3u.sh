253f41210447dcad29447ebb88c8e528b69e1934
mesb1 <b116d@mail.ru>
2021-07-25 22:58:49 +0300

    Update vbox1_rec2m3u.sh
    More fixes and checks.

--- vbox1_rec2m3u.sh
+++ vbox1_rec2m3u.sh
@@ -8,12 +8,15 @@ XMLMEM=$(curl -k -L -s "http://$VBOXIP/cgi-bin/HttpControl/HttpControlApp?OPTION
 
 #xmlstarlet sel -t -c "count(//ThreadGroup)" xmlfile
 
-TMPDATA0=$(echo $XMLMEM | xml sel -t -m "/record-list/record" -v "channel-name" -o "|" -v "programme-title" -o "|" -v "url" -n)
+TMPDATA0=$(echo $XMLMEM | xml sel -t -m "/record-list/record" -v "channel-name" -o "|" -v "programme-title" -o "|" -v "url" -o "|" -v "state" -n)
 TMPDATA3=$(echo $XMLMEM | xml sel -t -v '//url' -n)
 TMPDATA3=$(sed 's/127.0.0.1/'$VBOXIP'/g' <<< $TMPDATA3)
 
-readarray -t url <<<"$TMPDATA3"
-arraylength=${#url[@]}
+#readarray -t url <<<"$TMPDATA3"
+#arraylength=${#url[@]}
+
+readarray -t all <<<"$TMPDATA0"
+arraylength=${#all[@]}
 
 readarray -t -n $arraylength recorded <<<"$TMPDATA0"
 
@@ -21,11 +24,12 @@ M3UREADY="#EXTM3U"
 
 for (( i=0; i<${arraylength}; i++ ));
 do
-    IFS="|" read -r recitem1 recitem2 recitem3 <<<"${recorded[$i]}"
+    IFS="|" read -r recitem1 recitem2 recitem3 recitem4 <<<"${recorded[$i]}"
     if [ -z "$recitem2" ]; then recitem2="Unknown"; fi
+    if [ "$recitem4" != "recorded" ]; then continue; fi
 
     M3UREADY="${M3UREADY}\n#EXTINF:-1,""${recitem2} - ${recitem1}"
-    M3UREADY="${M3UREADY}\n${url[$i]}"
+    M3UREADY="${M3UREADY}\n${recitem3}"
 done
 
 echo -e $M3UREADY > $FINALPATH/$FINALM3U
